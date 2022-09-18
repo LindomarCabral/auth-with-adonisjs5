239d1b2e94b1399a0177f0780413683f3f54626e
LindomarCabral <lindomar.tecno@gmail.com>
2022-09-17 23:36:41 -0300

    feature user

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,10 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+
+export default class UsersController {
+  public async store({ request, response }: HttpContextContract) {
+    const userPayload = request.only(['email', 'username', 'password', 'avatar'])
+    const user = await User.create(userPayload)
+    return response.created({ user })
+  }
+}
--- tests/functional/users/user.spec.ts
+++ tests/functional/users/user.spec.ts
@@ -1,7 +1,13 @@
-import { test } from '@japa/runner'
+import Database from '@ioc:Adonis/Lucid/Database'
+import { Group, test } from '@japa/runner'
 
-test.group('Users user', () => {
-  test('it should create an user', async ({ client) => {
+test.group('Users user', (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+
+  test('it should create an user', async ({ client, assert }) => {
     const userPayload = {
       email: 'teste@teste.com',
       username: 'teste',
@@ -10,8 +16,28 @@ test.group('Users user', () => {
     }
     const response = await client.post('/users').json(userPayload)
 
-    // const { password, avatar, ...expected } = userPayload
+    const { password, avatar, ...expected } = userPayload
+    console.log(response.body().user)
 
     response.assertStatus(201)
+    response.assertBodyContains({ user: expected })
+    //assert.notEqual(response.body().user.password, userPayload.password)
+    assert.notExists(response.body().user.password, 'Password defined')
   })
+
+  // test('it should create an user 2', async ({ client, assert }) => {
+  //   const userPayload = {
+  //     email: 'teste@teste.com',
+  //     username: 'teste',
+  //     password: 'teste',
+  //     avatar: 'https://images.com/image/1',
+  //   }
+  //   const response = await client.post('/users').json(userPayload)
+
+  //   const { password, avatar, ...expected } = userPayload
+
+  //   response.assertStatus(201)
+  //   response.assertBodyContains({ user: expected })
+  //   assert.notExists(response.body().user.password, 'Password defined')
+  // })
 })
