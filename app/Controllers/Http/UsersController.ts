5e5751a817d3aacb258805c3203ad6c39f6e450b
LindomarCabral <lindomar.tecno@gmail.com>
2022-09-18 01:03:59 -0300

    add teste username alread

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -6,8 +6,10 @@ export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
     const userPayload = request.only(['email', 'username', 'password', 'avatar'])
     const userByEmail = await User.findBy('email', userPayload.email)
+    const userByUsername = await User.findBy('username', userPayload.username)
 
     if (userByEmail) throw new BadRequest('email already ins use', 409)
+    if (userByUsername) throw new BadRequest('username already ins use', 409)
 
     const user = await User.create(userPayload)
     return response.created({ user })
--- tests/functional/users/user.spec.ts
+++ tests/functional/users/user.spec.ts
@@ -31,13 +31,30 @@ test.group('Users user', (group) => {
       password: 'teste',
     }
     const response = await client.post('/users').json(userPayload)
-    console.log(response.body())
-
+    // console.log(response.body())
     response.assertStatus(409)
     assert.exists(response.body().message)
     assert.exists(response.body().code)
     assert.exists(response.body().status)
     assert.include(response.body().message, 'email')
+    assert.equal(response.body().code, 'BAD_REQUEST')
+    assert.equal(response.body().status, 409)
+  })
+
+  test('it should return 409 when username is already in use', async ({ client, assert }) => {
+    const { username } = await UserFactory.create()
+    const userPayload = {
+      email: 'teste@teste.com',
+      username,
+      password: 'teste',
+    }
+    const response = await client.post('/users').json(userPayload)
+    //console.log(response.body())
+    response.assertStatus(409)
+    assert.exists(response.body().message)
+    assert.exists(response.body().code)
+    assert.exists(response.body().status)
+    assert.include(response.body().message, 'username')
     assert.equal(response.body().code, 'BAD_REQUEST')
     assert.equal(response.body().status, 409)
   })
