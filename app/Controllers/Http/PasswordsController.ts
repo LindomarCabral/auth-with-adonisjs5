7fd7fed1fe915eee5a4f397e01d295fb36b6fd1d
LindomarCabral <lindomar.tecno@gmail.com>
2022-09-22 11:38:04 -0300

    password recovery and set

--- app/Controllers/Http/PasswordsController.ts
+++ app/Controllers/Http/PasswordsController.ts
@@ -1,7 +1,9 @@
 import Mail from '@ioc:Adonis/Addons/Mail'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import TokenExpired from 'App/Exceptions/TokenExpiredException'
 import User from 'App/Models/User'
 import ForgotPassword from 'App/Validators/ForgotPasswordValidator'
+import ResetPassword from 'App/Validators/ResetPasswordValidator'
 import { randomBytes } from 'crypto'
 import { promisify } from 'util'
 
@@ -31,6 +33,20 @@ export default class PasswordsController {
   }
 
   public async resetPassword({ request, response }: HttpContextContract) {
+    const { token, password } = await request.validate(ResetPassword)
+
+    const userByToken = await User.query()
+      .whereHas('tokens', (query) => {
+        query.where('token', token)
+      })
+      .preload('tokens')
+      .firstOrFail()
+    const tokenAge = Math.abs(userByToken.tokens[0].createdAt.diffNow('hours').hours)
+    if (tokenAge > 2) throw new TokenExpired()
+
+    userByToken.password = password
+    await userByToken.save()
+    await userByToken.tokens[0].delete()
     return response.noContent()
   }
 }
--- tests/functional/users/password.spec.ts
+++ tests/functional/users/password.spec.ts
@@ -1,8 +1,10 @@
+import Hash from '@ioc:Adonis/Core/Hash'
 import Mail from '@ioc:Adonis/Addons/Mail'
 import Database from '@ioc:Adonis/Lucid/Database'
 import { assert } from '@japa/preset-adonis'
 import { test } from '@japa/runner'
 import { UserFactory } from 'Database/factories'
+import { DateTime, Duration } from 'luxon'
 
 test.group('Passwords', (group) => {
   group.each.setup(async () => {
@@ -45,9 +47,42 @@ test.group('Passwords', (group) => {
   test('it should be able to reset password', async ({ assert, client }) => {
     const user = await UserFactory.create()
     const { token } = await user.related('tokens').create({ token: 'token' })
-
     const response = await client.post('/reset-password').json({ token, password: '123456' })
-
     response.assertStatus(204)
+    await user.refresh()
+    const checkPassword = await Hash.verify(user.password, '123456')
+    assert.isTrue(checkPassword)
+  })
+
+  test('it should return 422 when required data is not provided or data is invalid', async ({
+    assert,
+    client,
+  }) => {
+    const response = await client.post('/reset-password').json({})
+
+    response.assertStatus(422)
+    assert.equal(response.body().code, 'BAD_REQUEST')
+  })
+
+  test('it should return 404 when using the same token twice', async ({ client, assert }) => {
+    const user = await UserFactory.create()
+    const { token } = await user.related('tokens').create({ token: 'token' })
+    await client.post('/reset-password').json({ token, password: '123456' })
+    const response = await client.post('/reset-password').json({ token, password: '123456' })
+    response.assertStatus(404)
+    assert.equal(response.body().code, 'BAD_REQUEST')
+  })
+
+  test('it cannot reset password when token is expired after 2 hours', async ({
+    client,
+    assert,
+  }) => {
+    const user = await UserFactory.create()
+    const date = DateTime.now().minus(Duration.fromISOTime('02:01'))
+    const { token } = await user.related('tokens').create({ token: 'token', createdAt: date })
+    const response = await client.post('/reset-password').json({ token, password: '123456' })
+    response.assertStatus(410)
+    assert.equal(response.body().code, 'TOKEN_EXPIRED')
+    assert.equal(response.body().message, 'token has expired')
   })
 })
